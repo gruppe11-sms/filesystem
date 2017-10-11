ee5e18f7db44a09f509bd45184096ef03309c620
Rasmus Hansen <hansen13579@gmail.com>
2017-10-11 08:14:04 +0200

    Make project actually able to start

--- src/main/kotlin/dk/gruppe11/file/services/storage/FileProperties.kt
+++ src/main/kotlin/dk/gruppe11/file/services/storage/FileProperties.kt
@@ -1,6 +1,8 @@
 package dk.gruppe11.file.services.storage
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Configuration
 
+@Configuration
 @ConfigurationProperties("storage")
 data class FileProperties(var location : String = "storage")
