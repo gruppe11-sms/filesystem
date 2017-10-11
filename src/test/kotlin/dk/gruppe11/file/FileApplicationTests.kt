a24f432e28c74ffca409b60c8bca2f20acc2f725
Zlepper <hansen13579@gmail.com>
2017-10-11 19:15:14 +0200

    Ensure tests can be run

--- src/test/kotlin/dk/gruppe11/file/FileApplicationTests.kt
+++ src/test/kotlin/dk/gruppe11/file/FileApplicationTests.kt
@@ -2,11 +2,13 @@ package dk.gruppe11.file
 
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
+@DataJpaTest
 class FileApplicationTests {
 
 	@Test
